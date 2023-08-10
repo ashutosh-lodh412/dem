2868aa085c7939909efeeb6e6858353c86545f4f
ashutosh-lodh412 <125539020+ashutosh-lodh412@users.noreply.github.com>
2023-08-10 14:48:42 +0530

    Making some changes

--- pu.java
+++ pu.java
@@ -1,5 +1,5 @@
 class pu{
 public static void main(String[] args){
-System.out.println("Hello github this is my interaction with you");
+System.out.println("Hello github this is my interaction with you and git");
 }
 }
