add4a77dc50766c48db393cc71f50db49b9acaa0
ashutosh-lodh412 <125539020+ashutosh-lodh412@users.noreply.github.com>
2023-08-10 17:46:04 +0530

    InsertionSort

--- pu.java
+++ pu.java
@@ -1,4 +1,7 @@
 class pu{
+    static void insertionSort(){
+        System.out.println("Insertionsort");
+    }
 public static void main(String[] args){
 System.out.println("Hello github this is my interaction with you and git");
 System.out.println("The sum of 3+3 is 6");
